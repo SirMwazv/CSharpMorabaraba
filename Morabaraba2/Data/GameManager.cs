7206ef19bad990ae7d1dc0334b0af610309e3c68
3UpKing <tavonga6000@gmail.com>
2018-04-06 20:13:17 +0200

    Modify algorithm of RunPlacing method

--- Morabaraba2/Data/GameManager.cs
+++ Morabaraba2/Data/GameManager.cs
@@ -4,24 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Morabaraba2.Display;
+
 namespace Morabaraba2.Data
 {
     class GameManager
     {
         
-        public GameState state;
+        public GameState state;     //holds all the data of the current game state therefore access all player and game state variables through this field 
 
         /// <summary>
         /// Declare new Game Manager with predefined game state 
         /// </summary>
         /// <param name="state">New Game State to manage</param>
-        public GameManager(GameState state) //to be used for quickplay feature
+        public GameManager(GameState state)
         {
             this.state = state;
         }
 
         /// <summary>
-        /// Initializes Game if necesarry then runs the game.
+        /// Initializes Game and manages the game flow.
         /// </summary>
         public void RunGame()
         {
@@ -33,17 +35,18 @@ namespace Morabaraba2.Data
         /// </summary>
         void RunPlacing()
         {
-          /* Must have a list with all unused positions
-           * 1. Validate Placing:
-           * IF (UnusedPositionsList is empty) THEN 
-           *    Change State to Moving
-           *    ELSE
-           *        IF(Input is in the UnusedPositionsList) THEN 
-           *            Add Input to PlayerCowsList
-           *            Remove InputPosition from UnusedPositionsList
+          /* 
+           * 0. Use a while loop to run the phase e.g while (!checkPhase(state.phase)) therefore at the end of the loop the method wil check if the phase should move on
+           * Inside the while loop:
+               * 1. Prompt user for input and Get Input 
+               * 2.1 Validate input string is in correct format using IsValidInput Method
+               * 2.2 Validate Position is free using IsValidPosition
+               * 3.1 If validation fails print error using printErr method of board class and ask for input again (i recommend making an if statement that just prints the error, 
+               * reads the line so that the user can see it, then 'continue' to restart loop *Also: Important to clear the console at the beginning of the loop*)
+               * 3.2 If input is valid then add position to current players cow list e.g state.current.Cows.Add(GetPosition(inputFromUser));
+               * 4. swap players by calling SwapPlayer method of GameState class 
+            * End of loop (it will automatically check if it should move to the next phase)
            */
-            //TODO
-        
         }
 
         /// <summary>
